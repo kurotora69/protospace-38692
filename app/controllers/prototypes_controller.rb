f7e6a0278f37d9398cffc126b384b64fdab8a1de
kurotora69 <yuka.bftt@gmail.com>
2022-11-28 12:09:16 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,67 @@
+class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :move_to_index,  except: [:index, :show]
+
+  def index
+    @user = User.all
+    @prototypes = Prototype.includes(:user)
+  end 
+  
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end 
+    
+    @comment = Comment.new
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end  
+
+  def show
+    # @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments =  @prototype.comments.includes(:user)
+  end  
+
+  def edit
+    # @prototype = Prototype.find(params[:id])
+    @comment = Prototype.find(params[:id])
+  end
+
+  def update
+    # @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit 
+    end   
+  end  
+
+  def destroy
+    # @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+    
+  end
+  
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end 
+  
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end  
+
+end
